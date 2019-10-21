92a9253f207549e35b8e8c9a4bdea8196ed7412a
pavelklimov <pklimov100796@gmail.com>
2019-10-21 16:24:49 +0300

    Вычесляем кол во пулов WF

--- AutomatedВataСollection.sh
+++ AutomatedВataСollection.sh
@@ -2,7 +2,11 @@
 #переменные окружения
 #Путь до WildflyHome например /opt/wildfly
 WFHOME=/opt/wildfly
-
+# Суффикс ресурса для проверки доступности
+MyAppUri="cm5div6/api/"
+AuthToken=""
+# %занятых пулов см5 при которых срабатывает скрипт дефолтно 95
+dcm5=95
 # Ручной ввод закончен, дальше вычисляется автоматически. Править при необходимости
 STANDALONEXML=$WFHOME/standalone/configuration/standalone.xml
 #Получение ip сервера
@@ -18,9 +22,27 @@ if [ "$ip" = "0.0.0.0" ]
   then
     ip=127.0.0.1
   fi
+#определяем http port
+hport=`cat $STANDALONEXML |grep jboss.http.port |sed 's/ //g; s/jboss.http.port:/ /g; s/}/ /g' | awk '{print $2}'`
+#расчитываем офсет http порта
+let "hhtpport = ofset + hport"
+#формируем урл до api
+CHECKURL="http://"$ip":"$hhtpport"/"$MyAppUri
+#Опереляем пул соедений cm5
+cm5pool=`cat $STANDALONEXML |grep -A 30 'pool-name="CM5"' |grep "<max-pool-size>" |sed 's/</ /g; s/>/ /g' |awk '{print $2}'`
+#Опрелялем пул соденией cmj
+cmjpool=`cat $STANDALONEXML |grep -A 30  'pool-name="CMJ"' |grep "<max-pool-size>" |sed 's/</ /g; s/>/ /g' |awk '{print $2}'`
+#опредляем подклюсение к рсубд
 
 #Механизм обнаружение аварийного/сбойного состояния серверов
-
 #Получение данных о конектах
-$WFHOME/bin/jboss-cli.sh --connect --controller=$ip:$port --commands="/subsystem=datasources/xa-data-source=CM5/statistics=pool:read-resource(include-runtime=true)"
-$WFHOME/bin/jboss-cli.sh --connect --controller=$ip:$port --commands="/subsystem=datasources/xa-data-source=CMJ/statistics=pool:read-resource(include-runtime=true)"
+cm5a=`$WFHOME/bin/jboss-cli.sh --connect --controller=$ip:$port --commands="/subsystem=datasources/xa-data-source=CM5/statistics=pool:read-resource(include-runtime=true)" |grep ActiveCount |sed 's/,/ /g; s/>/ /g' |awk '{print $3}'`
+cmja=`$WFHOME/bin/jboss-cli.sh --connect --controller=$ip:$port --commands="/subsystem=datasources/xa-data-source=CMJ/statistics=pool:read-resource(include-runtime=true)" |grep ActiveCount |sed 's/,/ /g; s/>/ /g' |awk '{print $3}'`
+resultcm5=$(echo "$cm5a/$cm5pool" | bc -l)
+resultcm5pool=$(echo "$resultcm5*100" |bc -l )
+if (( $(echo "$resultcm5pool > $dcm5" |bc -l) ));
+then
+    echo "yes"
+else
+    echo "no"
+fi
