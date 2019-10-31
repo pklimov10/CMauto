1f5bfd9ec20f595b996c5fc394359a4200a662f4
pavelklimov <pklimov100796@gmail.com>
2019-10-31 17:22:21 +0300

    Автоматическое удлание задач из CSV

--- search.sh
+++ search.sh
@@ -3,6 +3,8 @@
 TOKEN=
 #id чата
 CHAT_ID=
+#Путь до WildflyHome например /opt/wildfly
+WFHOME=/opt/wildfly
 #утилита searcher-0.0.8.jar можно с путем /opt/searcher-0.0.8.jar
 searcher=/opt/select/searcher-0.0.8.jar
 #<путь до папки с файлом application.properties
@@ -27,9 +29,30 @@ dateFrom=$(date -d "1 day ago" +"%Y-%m-%d%%2000:00:00")
 #Формируем дату сегодня -3 часа
 dateTo=$(date +"%Y-%m-%d%%2000:00:00")
 nameCSV=$(date +"%Y-%m-%d-%H-%M").csv
-#опередляем джаву
+# Ручной ввод закончен, дальше вычисляется автоматически. Править при необходимости
+STANDALONEXML=$WFHOME/standalone/configuration/standalone.xml
+#Опредялем дравйер для РСУБД
+JDBCDRIVERNAME=`cat $STANDALONEXML |grep -A 30 'pool-name="CM5"'  |grep driver | sed 's/</ /g; s/>/ /g' |awk '{print $2}'`
+#Формируем путь до дравйера для РСУБД
+JDBCFILELOCATION=$WFHOME/standalone/deployments/$JDBCDRIVERNAME
+#получаем ip адрес cm5
+IP_CM5=`cat $STANDALONEXML |grep -A 30 'pool-name="CM5"' |grep jdbc:postgresql | sed 's/ //g'   |sed 's/\/\// /' |awk '{print $2}'  | sed 's/:/ /;s/\// /g' |awk '{print $1}'`
+#Получаем порт cm5
+PORT_CM5=`cat $STANDALONEXML |grep -A 30 'pool-name="CM5"' |grep jdbc:postgresql | sed 's/ //g'   |sed 's/\/\// /' |awk '{print $2}'  | sed 's/:/ /;s/\// /g' |awk '{print $2}'`
+#Получаем название базы cm5
+DB_CN5_NAME=`cat $STANDALONEXML |grep -A 30 'pool-name="CM5"' |grep jdbc:postgresql | sed 's/ //g'   |sed 's/\/\// /' |awk '{print $2}'  | sed 's/:/ /;s/\// /g' | sed 's/?/ /g' |awk '{print $3}'`
+#полчаем юзера для РСУБД CM5
+DB_CM5_USER=`cat $STANDALONEXML |grep -A 30 'pool-name="CM5"' |grep user-name  | sed 's/ //g' |sed  's/</ /g; s/>/ /g' |awk '{print $2}'`
+#Получаем пароль для РСУБД cm5
+DB_CM5_PASS=`cat $STANDALONEXML |grep -A 30 'pool-name="CM5"' |grep password  | sed 's/ //g' |sed  's/</ /g; s/>/ /g' |awk '{print $2}'`
 #провряем путь до javahome systemctl status wildfly | grep Standalone  |awk '{print $2}'
 my_java_home=`systemctl status wildfly | grep Standalone  |awk '{print $2}'`
+echo $JDBCFILELOCATION "Драйдевер для рсубд"
+echo $IP_CM5 "ip cm5"
+echo $PORT_CM5 "Получаем порт cm5"
+echo $DB_CN5_NAME "Получаем название базы cm5"
+echo $DB_CM5_USER "полчаем юзера для РСУБД CM5"
+echo $DB_CM5_PASS "Получаем пароль для РСУБД cm5"
 #вормируем CSV
 cd $properties
 csv_dir=$my_dir_csv/$nameCSV
@@ -39,3 +62,8 @@ my_kill=$(ps -ef | grep java |grep $searcher |awk '{print $2}')
 echo $my_kill
 kill -9 $my_kill
 curl -F chat_id=$CHAT_ID -F document=@"$csv_dir" -F caption="CSV" http://185.112.82.9:85/bot$TOKEN/sendDocument
+RSUBD_CM5=`$my_java_home -cp $JDBCFILELOCATION":/"  PostgresqlQueryExecuteJDBC  -h $IP_CM5 -p $PORT_CM5 -U $DB_CM5_USER -W $DB_CM5_PASS -d $DB_CN5_NAME -c "select replica from ss_module where type in (select id  from ss_moduletype where alias like '%Prev%');" |grep -v replica `
+for ADDR in ${RSUBD_CM5[*]}
+        do
+         sed -i "/$ADDR/d" $my_dir_csv/$nameCSV
+        done
