c83585f9b3fbd041d010bcf9ed76d3b7962e781b
pavelklimov <pklimov100796@gmail.com>
2019-11-01 15:47:45 +0300

    CHlog

--- changeLogs.sh
+++ changeLogs.sh
@@ -2,6 +2,8 @@
 #переменные окружения
 #Путь до WildflyHome например /opt/wildfly
 WFHOME=/opt/wildfly
+#путь до дректории куда пишем лог
+log_dir=/opt/log
 lastday=$(date -d '1 day ago' +'%Y-%m-%d')
 # Ручной ввод закончен, дальше вычисляется автоматически. Править при необходимости
 STANDALONEXML=$WFHOME/standalone/configuration/standalone.xml
@@ -29,4 +31,11 @@ echo $DB_CM5_USER "полчаем юзера для РСУБД CM5"
 echo $DB_CM5_PASS "Получаем пароль для РСУБД cm5"
 #проверяем чейнж логи
 RSUBD_CM5=`$my_java_home -cp $JDBCFILELOCATION":/"  PostgresqlQueryExecuteJDBC  -h $IP_CM5 -p $PORT_CM5 -U $DB_CM5_USER -W $DB_CM5_PASS -d $DB_CN5_NAME -c "select a.description, count(a.description) from ag_data_message am join ag_agent a on am.agent=a.id where am.excluded = 1 and am.created_date > '$lastday' group by a.description order by description asc" |grep -v description`
-echo $RSUBD_CM5
+echo $RSUBD_CM5 > $log_dir/$(date +"%Y-%m-%d-%H-%M").csv
+if [[ ! -z $RSUBD_CM5 ]];
+then
+    echo "yes" (есть строки) > $log_dir/$(date +"%Y-%m-%d-%H-%M").csv
+    $my_java_home -cp $JDBCFILELOCATION":/"  PostgresqlQueryExecuteJDBC  -h $IP_CM5 -p $PORT_CM5 -U $DB_CM5_USER -W $DB_CM5_PASS -d $DB_CN5_NAME -c "select a.description, count(a.description) from ag_data_message am join ag_agent a on am.agent=a.id where am.excluded = 1 and am.created_date > '$lastday'  group by a.description order by description asc"
+else
+    echo "no" (СТрок нет) > $log_dir/$(date +"%Y-%m-%d-%H-%M").csv
+fi
